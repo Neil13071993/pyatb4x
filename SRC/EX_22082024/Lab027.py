450ee79c5e3799dd0aa694be314d5444789c4ff8
Nilesh Gaikwad <ng5779@gmail.com>
2024-09-10 10:43:47 -0700

    add all file - 24th aug

--- SRC/EX_22082024/Lab027.py
+++ SRC/EX_22082024/Lab027.py
@@ -2,9 +2,11 @@ user_type = input("Enter the user type, admin,manager, guest \n")
 match user_type:
     case "admin" :
         print("Hello Sir")
-    case "manager":
+    case "manager" | "guest":
         print("Hello manager")
     case "guest":
         print("Hello guest")
     case _:
         print(" Whats up bro")
+
+        # we can use | for multiple options.
