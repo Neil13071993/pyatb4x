cb80099256612d036d67a127544dc7db5cc60640
Nilesh Gaikwad <ng5779@gmail.com>
2024-09-26 11:28:22 -0700

    add all file - 29th aug

--- SRC/EX_29082024/Lab058.py
+++ SRC/EX_29082024/Lab058.py
@@ -10,3 +10,24 @@ print(student_info["address"])
 
 student_info["age"] = 100
 print(student_info)
+
+student_info1 = {
+    "name" : "nilesh",
+    "age" : 65,
+    "address" : {
+        "Home address" : "pune",
+        "office address" : "Chinchwad"
+    }
+}
+
+student_info2 = {
+    "name" : "yogesh",
+    "age" : 55,
+    "address" : {
+        "Home address" : "Chinchwad",
+        "office address" : "pune"
+    }
+}
+
+student_list = [student_info1,student_info2]
+print(student_list)
